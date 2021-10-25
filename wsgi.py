f5d92a29d1cfbd87ae3629ac19333324252f9f2f
Alex <dexpiper@yandex.ru>
2021-10-25 21:52:24 +0300

    Handlers again

--- wsgi.py
+++ wsgi.py
@@ -59,11 +59,9 @@ def user_request(message):
 #
 @app.route('/' + TOKEN, methods=['POST'])
 def getMessage():
-    bot.process_new_updates(
-        [telebot.types.Update.de_json(
-            request.stream.read().decode("utf-8")
-            )]
-    )
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
     return "!", 200
 
 
