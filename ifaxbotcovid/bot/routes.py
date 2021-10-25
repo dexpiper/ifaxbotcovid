928ca23a764b53a7e1820ef7a756276de78427db
Alex <dexpiper@yandex.ru>
2021-10-25 18:20:47 +0300

    Dealing with bot handlers

--- ifaxbotcovid/bot/routes.py
+++ ifaxbotcovid/bot/routes.py
@@ -18,34 +18,32 @@ with app.app_context():
     TOKEN = current_app.config['TELEGRAM_TOKEN']
     main = Blueprint('main', __name__)
 
+    #
+    # MESSAGE HANDLERS
+    #
+    @bot.message_handler(commands=['start'])
+    def answer_start(message):
+        '''
+        Bot sends welcome message
+        '''
+        bot.send_message(message.chat.id, startmessage.s, parse_mode='HTML')
+        logger.info(
+            'User %s issued "start" command' % message.from_user.username)
+        user = message.from_user.username
+        chat_id = message.chat.id
+        if (user, chat_id) not in settings.users:
+            settings.users.append((user, chat_id))
 
-#
-# MESSAGE HANDLERS
-#
-@bot.message_handler(commands=['start'])
-def answer_start(message):
-    '''
-    Bot sends welcome message
-    '''
-    bot.send_message(message.chat.id, startmessage.s, parse_mode='HTML')
-    logger.info(
-        'User %s issued "start" command' % message.from_user.username)
-    user = message.from_user.username
-    chat_id = message.chat.id
-    if (user, chat_id) not in settings.users:
-        settings.users.append((user, chat_id))
-
-
-@bot.message_handler(content_types=['text'])
-def user_request(message):
-    logger.info('User %s send some text' % message.from_user.username)
-    answer = chef.process_new_message(message)
-    if answer.flag:
-        if answer.warnmessage:
-            bot.send_message(message.chat.id, answer.warnmessage)
-        bot.send_message(message.chat.id, answer.ready_text)
-        if message.text.endswith('йй'):
-            bot.send_message(message.chat.id, answer.log)
+    @bot.message_handler(content_types=['text'])
+    def user_request(message):
+        logger.info('User %s send some text' % message.from_user.username)
+        answer = chef.process_new_message(message)
+        if answer.flag:
+            if answer.warnmessage:
+                bot.send_message(message.chat.id, answer.warnmessage)
+            bot.send_message(message.chat.id, answer.ready_text)
+            if message.text.endswith('йй'):
+                bot.send_message(message.chat.id, answer.log)
 
 
 #
