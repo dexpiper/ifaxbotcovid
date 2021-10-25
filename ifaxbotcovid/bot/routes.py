b41ec1e21b85d97d8f9c08624640ebd05d2156a2
Alex <dexpiper@yandex.ru>
2021-10-25 18:25:56 +0300

    Dealing with bot handlers2

--- ifaxbotcovid/bot/routes.py
+++ ifaxbotcovid/bot/routes.py
@@ -4,9 +4,6 @@ from flask import request
 
 import telebot
 
-import ifaxbotcovid.config.startmessage as startmessage
-import ifaxbotcovid.config.settings as settings
-
 
 app = current_app
 
@@ -17,33 +14,6 @@ with app.app_context():
     URL = current_app.config['TELEBOT_URL']
     TOKEN = current_app.config['TELEGRAM_TOKEN']
     main = Blueprint('main', __name__)
-
-    #
-    # MESSAGE HANDLERS
-    #
-    @bot.message_handler(commands=['start'])
-    def answer_start(message):
-        '''
-        Bot sends welcome message
-        '''
-        bot.send_message(message.chat.id, startmessage.s, parse_mode='HTML')
-        logger.info(
-            'User %s issued "start" command' % message.from_user.username)
-        user = message.from_user.username
-        chat_id = message.chat.id
-        if (user, chat_id) not in settings.users:
-            settings.users.append((user, chat_id))
-
-    @bot.message_handler(content_types=['text'])
-    def user_request(message):
-        logger.info('User %s send some text' % message.from_user.username)
-        answer = chef.process_new_message(message)
-        if answer.flag:
-            if answer.warnmessage:
-                bot.send_message(message.chat.id, answer.warnmessage)
-            bot.send_message(message.chat.id, answer.ready_text)
-            if message.text.endswith('йй'):
-                bot.send_message(message.chat.id, answer.log)
 
 
 #
--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,7 @@ import logging
 
 from ifaxbotcovid.bot import factory
 from ifaxbotcovid.config import settings
+import ifaxbotcovid.config.startmessage as startmessage
 
 
 TOKEN = os.environ.get('TOKEN')
@@ -26,6 +27,36 @@ app.config['TELEBOT_LOGGER'] = logger
 app.config['BOT_CHEF'] = chef
 app.config['TELEBOT_URL'] = URL
 app.config['TELEGRAM_TOKEN'] = TOKEN
+
+
+#
+# MESSAGE HANDLERS
+#
+@bot.message_handler(commands=['start'])
+def answer_start(message):
+    '''
+    Bot sends welcome message
+    '''
+    bot.send_message(message.chat.id, startmessage.s, parse_mode='HTML')
+    logger.info(
+        'User %s issued "start" command' % message.from_user.username)
+    user = message.from_user.username
+    chat_id = message.chat.id
+    if (user, chat_id) not in settings.users:
+        settings.users.append((user, chat_id))
+
+
+@bot.message_handler(content_types=['text'])
+def user_request(message):
+    logger.info('User %s send some text' % message.from_user.username)
+    answer = chef.process_new_message(message)
+    if answer.flag:
+        if answer.warnmessage:
+            bot.send_message(message.chat.id, answer.warnmessage)
+        bot.send_message(message.chat.id, answer.ready_text)
+        if message.text.endswith('йй'):
+            bot.send_message(message.chat.id, answer.log)
+
 
 with app.app_context():
 
